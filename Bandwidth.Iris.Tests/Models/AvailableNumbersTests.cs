9de91e13c8a03530ad8fa9ef583153d3c8622fe0
Andrey Belchikov <belchikovs@gmail.com>
2014-11-13 09:59:34 +0300

    Added AvailableNumbers

---
+++ Bandwidth.Iris.Tests/Models/AvailableNumbersTests.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using Bandwidth.Iris.Model;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Bandwidth.Iris.Tests.Models
+{
+    [TestClass]
+    public class AvailableNumbersTests
+    {
+        [TestInitialize]
+        public void Setup()
+        {
+            Helper.SetEnvironmetVariables();
+        }
+
+        [TestMethod]
+        public void ListTest()
+        {
+            using (var server = new HttpServer(new RequestHandler
+            {
+                EstimatedMethod = "GET",
+                EstimatedPathAndQuery = string.Format("/v1.0/accounts/{0}/availableNumbers?areaCode=866&quantity=5", Helper.AccountId),
+                ContentToSend = Helper.CreateXmlContent(new AvailableNumbersResult
+                {
+                    ResultCount = 2,
+                    TelephoneNumberList = new[] { "1111", "2222"}
+                })
+            }))
+            {
+                var client = Helper.CreateClient();
+                var result = AvailableNumbers.List(client, new Dictionary<string, object>
+                {
+                    {"areaCode", 866},
+                    {"quantity", 5}
+                }).Result;
+                if (server.Error != null) throw server.Error;
+                Assert.AreEqual(2, result.ResultCount);
+                CollectionAssert.AreEqual(new[] { "1111", "2222" }, result.TelephoneNumberList);
+            }
+        }
+    }
+}
---
+++ Bandwidth.Iris/Model/AvailableNumbers.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Bandwidth.Iris.Model
+{
+    public class AvailableNumbers
+    {
+        private const string AvailableNumbersPath = "availableNumbers";
+        public static Task<AvailableNumbersResult> List(Client client, IDictionary<string, object> query = null)
+        {
+            return client.MakeGetRequest<AvailableNumbersResult>(client.ConcatAccountPath(AvailableNumbersPath), query);
+        }
+    }
+
+    [XmlType("SearchResult")]
+    public class AvailableNumbersResult
+    {
+        public int ResultCount { get; set; }
+        
+        [XmlArrayItem("TelephoneNumber")]
+        public string[] TelephoneNumberList { get; set; }
+
+        public TelephoneNumberDetail[] TelephoneNumberDetailList { get; set; }
+    }
+
+    public class TelephoneNumberDetail
+    {
+        public string City { get; set; }
+
+        [XmlElement("LATA")]
+        public string Lata { get; set; }
+        public string RateCenter { get; set; }
+        public string State { get; set; }
+        public string FullNumber { get; set; }
+        public string Tier { get; set; }
+        public string VendorId { get; set; }
+        public string VendorName { get; set; }
+    }
+}
