e7b202c0ae7712661d83c0ae4215e540b8090ef7
Andrey Belchikov <belchikovs@gmail.com>
2014-11-14 09:18:30 +0300

    Added Disconnect

---
+++ Bandwidth.Iris.Tests/Models/DisconnectTests.cs
@@ -0,0 +1,62 @@
+﻿using Bandwidth.Iris.Model;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Bandwidth.Iris.Tests.Models
+{
+    [TestClass]
+    public class DisconnectTests
+    {
+        [TestInitialize]
+        public void Setup()
+        {
+            Helper.SetEnvironmetVariables();
+        }
+
+        [TestMethod]
+        public void DisconnectNumbersTest()
+        {
+            var data = new DisconnectTelephoneNumberOrder
+            {
+                Name = "order",
+                DisconnectTelephoneNumberOrderType = new DisconnectTelephoneNumberOrderType
+                {
+                    TelephoneNumberList = new[] {"111", "222"}
+                }
+            };
+            using (var server = new HttpServer(new RequestHandler
+            {
+                EstimatedMethod = "POST",
+                EstimatedPathAndQuery = string.Format("/v1.0/accounts/{0}/disconnects", Helper.AccountId),
+                EstimatedContent = Helper.ToXmlString(data)
+            }))
+            {
+                var client = Helper.CreateClient();
+                Disconnect.DisconnectNumbers(client, "order", "111", "222").Wait();
+                if (server.Error != null) throw server.Error;
+            }
+        }
+
+        public void DisconnectNumbersWithDefaultClientTest()
+        {
+            var data = new DisconnectTelephoneNumberOrder
+            {
+                Name = "order",
+                DisconnectTelephoneNumberOrderType = new DisconnectTelephoneNumberOrderType
+                {
+                    TelephoneNumberList = new[] { "111", "222" }
+                }
+            };
+            using (var server = new HttpServer(new RequestHandler
+            {
+                EstimatedMethod = "POST",
+                EstimatedPathAndQuery = string.Format("/v1.0/accounts/{0}/disconnects", Helper.AccountId),
+                EstimatedContent = Helper.ToXmlString(data)
+            }))
+            {
+                Disconnect.DisconnectNumbers("order", "111", "222").Wait();
+                if (server.Error != null) throw server.Error;
+            }
+        }
+      
+    }
+}
---
+++ Bandwidth.Iris/Model/Disconnect.cs
@@ -0,0 +1,41 @@
+﻿using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Bandwidth.Iris.Model
+{
+    public class Disconnect
+    {
+        private const string DisconnectNumbersPath = "disconnects";
+        public static Task DisconnectNumbers(Client client, string orderName, params string[] numbers)
+        {
+            var order = new DisconnectTelephoneNumberOrder
+            {
+                Name = orderName,
+                DisconnectTelephoneNumberOrderType = new DisconnectTelephoneNumberOrderType
+                {
+                    TelephoneNumberList = numbers
+                }
+            };
+            return client.MakePostRequest(client.ConcatAccountPath(DisconnectNumbersPath), order, true);
+        }
+#if !PCL
+        public static Task DisconnectNumbers(string orderName, params string[] numbers)
+        {
+            return DisconnectNumbers(Client.GetInstance(), orderName, numbers);
+        }
+#endif
+    }
+
+    public class DisconnectTelephoneNumberOrder
+    {
+        [XmlElement("name")]
+        public string Name { get; set; }
+        public DisconnectTelephoneNumberOrderType DisconnectTelephoneNumberOrderType { get; set; }
+    }
+
+    public class DisconnectTelephoneNumberOrderType
+    {
+        [XmlArrayItem("TelephoneNumber")]
+        public string[] TelephoneNumberList { get; set; }
+    }
+}
