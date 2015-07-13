3c2efe068abd022cc6f50fdd9b4851a2bee1b78b
Scott Barstow <sbarstow@gmail.com>
2015-07-13 08:52:14 -0400

    added few missing properties

--- Bandwidth.Iris/Model/LnpChecker.cs
+++ Bandwidth.Iris/Model/LnpChecker.cs
@@ -26,6 +26,7 @@ namespace Bandwidth.Iris.Model
     {
         public RateCenterGroup[] SupportedRateCenters { get; set; }
         public RateCenterGroup[] UnsupportedRateCenters { get; set; }
+        public RateCenterGroup[] PartnerSupportedRateCenters { get; set; }
         [XmlArrayItem("Tn")]
         public string[] PortableNumbers { get; set; }
         public LosingCarriers SupportedLosingCarriers { get; set; }
