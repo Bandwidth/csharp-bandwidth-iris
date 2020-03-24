93bf986834c87fa98901395e8ae74dfcc2643adb
John Chavez <jchavez@bandwidth.com>
2020-03-24 15:23:12 -0400

    Updated versions

--- Bandwidth.Iris/Properties/AssemblyInfo.cs
+++ Bandwidth.Iris/Properties/AssemblyInfo.cs
@@ -27,8 +27,8 @@ using System.Runtime.CompilerServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.3.0")]
-[assembly: AssemblyFileVersion("1.3.0")]
+[assembly: AssemblyVersion("1.3.1")]
+[assembly: AssemblyFileVersion("1.3.1")]
 
 #if DEBUG
 
