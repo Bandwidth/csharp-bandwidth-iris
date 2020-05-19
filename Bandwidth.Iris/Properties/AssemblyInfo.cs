a222303265bba16b218800b6408f9d15527d69f6
John Chavez <jchavez@bandwidth.com>
2020-05-19 13:05:17 -0400

    breaking change

--- Bandwidth.Iris/Properties/AssemblyInfo.cs
+++ Bandwidth.Iris/Properties/AssemblyInfo.cs
@@ -27,8 +27,8 @@ using System.Runtime.CompilerServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.4.0")]
-[assembly: AssemblyFileVersion("1.4.0")]
+[assembly: AssemblyVersion("2.0.0")]
+[assembly: AssemblyFileVersion("2.0.0")]
 
 #if DEBUG
 
