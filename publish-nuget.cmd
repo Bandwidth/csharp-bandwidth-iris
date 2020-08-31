064747753307d51d1839209c64d62a6c6db5a6ee
John Chavez <52748091+jchavez443@users.noreply.github.com>
2020-08-31 11:05:12 -0400

    Added AreaCode (#49)

--- publish-nuget.cmd
+++ publish-nuget.cmd
@@ -2,6 +2,8 @@
 
 del *.nupkg
 
+dotnet build --configuration Release
+
 SET /p apiKey=Input the Nuget API Key:  
 .\.nuget\nuget pack -Properties Configuration=Release && dotnet nuget push Bandwidth.Iris.*.nupkg -s nuget.org -k %apiKey%
 
