2dd0d56adaf22ef42a5743fb99eb5cdeae6df8cc
Mike Hamer <mhamer@bandwidth.com>
2021-09-28 11:40:45 -0400

    Bump version

--- Bandwidth.Iris/Properties/AssemblyInfo.cs
+++ Bandwidth.Iris/Properties/AssemblyInfo.cs
@@ -27,8 +27,8 @@ using System.Runtime.CompilerServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("2.3.0")]
-[assembly: AssemblyFileVersion("2.3.0")]
+[assembly: AssemblyVersion("2.3.1")]
+[assembly: AssemblyFileVersion("2.3.1")]
 
 #if DEBUG
 
